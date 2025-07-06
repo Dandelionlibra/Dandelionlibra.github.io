2f843d3bd2cdbd170886f3cd5df55cf937e786dc
Dandelionlibra <Dandelionlibra@users.noreply.github.com>
2025-07-06 16:55:20 +0000

    Updated tutorial on how to use langchain 35cc0a86848b0b277ebe6eb0d1836326978cae1e

--- post/langchain/langchain_practice1.py
+++
@@ -1,7 +0,0 @@
-from langchain_ollama import OllamaLLM
-
-llm = OllamaLLM(model="llama3.1:8b")
-
-# response = llm.invoke("幫我取一個文雅的中國男孩名")
-response = llm.invoke("幫我取一個文雅的中國男孩名", options={"temperature": 0})
-print(response)
