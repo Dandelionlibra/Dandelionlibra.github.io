936f0eac6082b8d3cddecce1d03abc650ebd7a33
Dandelionlibra <Dandelionlibra@users.noreply.github.com>
2025-07-17 02:33:27 +0000

    fix build fail 3e3d18bdeb23224e33b801382fb1617d78335adc

--- post/langchain/LangChain_Pdf_loader.py
+++ post/langchain/LangChain_Pdf_loader.py
@@ -41,7 +41,9 @@ print("[4] hello.")
 
 retriever = db.as_retriever()
 query = "小王子"
-docs = retriever.get_relevant_documents(query)
+# The method `BaseRetriever.get_relevant_documents` was deprecated in langchain-core 0.1.46 and will be removed in 1.0. Use :meth:`~invoke` instead.
+# docs = retriever.get_relevant_documents(query)
+docs = retriever.invoke(query)
 print("[5] hello.")
 
 print(docs[0].page_content)
@@ -52,53 +54,7 @@ print("[6] hello.")
 
 
 
+'''
 
 
-
-
-
-
-
-
-
-
-# 分割文件內容
-text_splitter = RecursiveCharacterTextSplitter(
-    # set the chunk size
-    chunk_size=100, # 每個 chunk 的最大字元數
-    chunk_overlap=20, # 每個 chunk 之間的重疊字元數
-    length_function=len # 用於計算字元長度的函數，默認為 len
-)
-
-
-# load_and_split 可在加載時提供文本分割器
-pages = loader.load_and_split(text_splitter=text_splitter)  
-
-
-
-
-# 將文檔寫入 ChromaDB
-vector_store = Chroma.from_documents(
-    documents,
-    embedding=embeddings_model,
-    persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
-)
-
-
-# # vector_store.persist()
-
-
-# # # db 即是向量儲存庫，可用於相似度檢索
-# # db = Chroma.from_documents(pages, embeddings_model)
-
-
-# query = "小王子"
-
-# # k=3 表示返回最相似的3個文檔
-# results = vector_store.similarity_search(query, k=3)
-
-# print(f"找到 {len(results)} 個相關文檔\n")
-
-# print(f"\n找到 {len(results)} 個相關文檔：\n")
-# for i, doc in enumerate(results, 1):
-#     print(f"[{i}] {doc.page_content}\n---\n")
+'''
